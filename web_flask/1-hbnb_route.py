410ec1db05c48fad7ba21321a25807f74c3d2e75
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-24 13:28:20 +0100

    Defining new route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Script that starts a simple Flask Web Application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Returns a View that displays "Hello HBNB! For the root URL route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbhb', strict_slashes=False)
+def hbnb_route():
+    """Returns a View that displays "HBNB" for the /hbnb URL route"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
